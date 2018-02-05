ba72d0a5ebf07dfb53461569f50d32f216c7fd99
Petr Němeček <thenemecek@gmail.com>
2018-02-05 23:10:59 +0100

    Fixed loading env vars in dev and production

--- modules/env.js
+++ modules/env.js
@@ -1,30 +1,29 @@
 "use strict";
 
-const
+let env = {};
+
+if (process.env.NODE_ENV === "production") {
+  env = process.env;
+} else {
   env = require("../config/env.json");
+}
 
 // App Secret can be retrieved from the App Dashboard
-exports.APP_SECRET = process.env.MESSENGER_APP_SECRET ||
-                     env.MESSENGER_APP_SECRET;
+exports.APP_SECRET = env.MESSENGER_APP_SECRET;
 
 // Arbitrary value used to validate a webhook
-exports.VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN ||
-                           env.MESSENGER_VALIDATION_TOKEN;
+exports.VALIDATION_TOKEN = env.MESSENGER_VALIDATION_TOKEN;
 
 // Generate a page access token for your page from the App Dashboard
-exports.PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN ||
-                            env.MESSENGER_PAGE_ACCESS_TOKEN;
+exports.PAGE_ACCESS_TOKEN = env.MESSENGER_PAGE_ACCESS_TOKEN;
 
 // URL where the app is running (include protocol). Used to point to scripts and
 // assets located at this address.
-exports.SERVER_URL = process.env.SERVER_URL ||
-                     env.SERVER_URL;
+exports.SERVER_URL = env.SERVER_URL;
 
-exports.DB_URL = process.env.DB_URL ||
-                 env.DB_URL;
+exports.DB_URL = env.DB_URL;
 
-exports.CIPHER_KEY = process.env.CIPHER_KEY ||
-                     env.CIPHER_KEY;
+exports.CIPHER_KEY = env.CIPHER_KEY;
 
 if (!(this.APP_SECRET &&
       this.VALIDATION_TOKEN &&
